fa1dd4d2de9283cacce8237fc3f9199abbfe9d84
Francescodl04 <72445602+Francescodl04@users.noreply.github.com>
2021-10-06 15:53:33 +0200

    Completamento TemperaturaCittà

--- TemperaturaCittà/TemperaturaCittà/Program.cs
+++ TemperaturaCittà/TemperaturaCittà/Program.cs
@@ -14,42 +14,41 @@ namespace TemperaturaCittà
     class Temperature
     {
         double[] TemperatureCittà { get; set; }
-        string NomeCittà { get; set; }
         public Temperature(double [] TemperatureCittà)
         {
             this.TemperatureCittà = TemperatureCittà;
         }
-        public void LetturaTemperature()
-        {
-            Console.WriteLine("Benvenuto nel programma. Inserisci il nome della città:");
-            NomeCittà = Console.ReadLine();
-            Console.WriteLine("\nInserisci il numero di temperature");
-            int numeroTemperature = int.Parse(Console.ReadLine());
-            TemperatureCittà = new double[numeroTemperature];
-            for (int i = 0; i < numeroTemperature; i++)
-            {
-                Console.WriteLine($"Inserisci la {i + 1}a temperatura:");
-                TemperatureCittà[i] = double.Parse(Console.ReadLine());
-            }
-        }
-        public void OrdinaTemperature()
+        public double [] OrdinaTemperature()
         {
             Array.Sort(TemperatureCittà);
-        }
-        public void MostraRisultati()
-        {
-            for (int j = 0, j < TemperatureCittà.Length; j++)
-            {
-                Console.WriteLine($"{j + 1}) {TemperatureCittà[i]}");
-            }
+            return TemperatureCittà;
         }
     }
 
     class Program
     {
+        static double[] TemperatureCittà;
+        static string nomeCittà;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Benvenuto nel programma. Inserisci il nome della città:");
+            nomeCittà = Console.ReadLine();
+            Console.WriteLine("\nInserisci il numero di temperature");
+            int numeroTemperature = int.Parse(Console.ReadLine());
+            TemperatureCittà = new double[numeroTemperature];
+            for (int i = 0; i < numeroTemperature; i++)
+            {
+                Console.WriteLine($"\nInserisci la {i + 1}a temperatura:");
+                TemperatureCittà[i] = double.Parse(Console.ReadLine());
+            }
+            Temperature t = new Temperature(TemperatureCittà);
+            Console.WriteLine($"\nLe temperature di {nomeCittà} (gradi centigradi) sono, ordinate in modo crescente: ");
+            for (int i = 0; i < TemperatureCittà.Length; i++)
+            {
+                Console.WriteLine($"\n{i + 1}) {t.OrdinaTemperature()[i]}.");
+            }
+            Console.WriteLine("\nPer uscire dal programma premere un tasto qualsiasi...");
+            Console.ReadKey();
         }
     }
 }
